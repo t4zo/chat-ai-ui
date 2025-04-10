e7dac9d91133550541647bccbec13efe8bc9e545
Tacio de Souza Campos <taciodesouzacampos@gmail.com>
2025-04-10 10:16:06 -0300

    fix scroll to bottom

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,18 +1,14 @@
 <script lang="ts" setup>
-import { useChatStore, type FormattedMessage } from "../stores/chat"
+import { useChatStore } from "../stores/chat"
 import Message from "./Message.vue"
 
 const chatStore = useChatStore()
-
-function isUserMessage(message: FormattedMessage) {
-  return chatStore.isUserMessage(message)
-}
 </script>
 
 <template>
-  <div id="chatContainer" class="flex-1 overflow-y-auto p-4 space-y-12">
-    <div v-for="(message, index) in chatStore.messages" :class="`flex ${isUserMessage(message) ? 'justify-end' : 'justify-start'}`">
-      <Message :key="index" :isUserMessage="isUserMessage(message)" :content="message.content" />
+  <div class="flex-1 overflow-y-auto p-4 space-y-12">
+    <div v-for="(message, index) in chatStore.messages" :class="`flex ${chatStore.isUserMessage(message) ? 'justify-end' : 'justify-start'}`">
+      <Message :key="index" :isUserMessage="chatStore.isUserMessage(message)" :content="message.content" />
     </div>
   </div>
 </template>
